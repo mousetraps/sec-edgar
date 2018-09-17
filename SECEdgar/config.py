5f5199bf95bf6c0ef930b7cee5c26a7122118d37
Sara Itani <itani@outlook.com>
2018-09-16 21:28:52 -0700

    make DEFAULT_DATA_PATH current directory

--- SECEdgar/config.py
+++ SECEdgar/config.py
@@ -5,4 +5,4 @@ import logging
 import os
 
 DEFAULT_DATA_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'SEC-Edgar-Data'))
+    os.path.dirname(os.getcwd()), 'SEC-Edgar-Data'))
