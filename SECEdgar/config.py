099370da8cd38d1599dffbe92a2d5df670827716
Sara Itani <itani@outlook.com>
2018-09-16 21:31:49 -0700

    add option to set DEFAULT_DATA_PATH via env variable

--- SECEdgar/config.py
+++ SECEdgar/config.py
@@ -4,5 +4,5 @@
 import logging
 import os
 
-DEFAULT_DATA_PATH = os.path.abspath(os.path.join(
+DEFAULT_DATA_PATH = os.environ.get('SEC_DATA_DIR') or os.path.abspath(os.path.join(
     os.path.dirname(os.getcwd()), 'SEC-Edgar-Data'))
